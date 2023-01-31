828bd96cb6078c6efe4656a5e7efcace916c70f8
liu <2506991786@qq.com>
2023-02-01 02:11:40 +0800

    anew

--- test/UniswapVPool.sol
+++ test/UniswapVPool.sol
@@ -29,22 +29,6 @@ import "forge-std/Test.sol";
         token0 = new ERC20Mintable("Ether","ETH",18);        
         token1 = new ERC20Mintable("USDC","USDC",18);
     }
-
-
-    function testMintSuccess() public pure returns(TestCaseParams memory params) {
-        params = TestCaseParams({
-        wethBalance: 1 ether,
-        usdcBalance: 5000 ether,
-        currentTick: 85176,
-        lowerTick: 84222,
-        upperTick: 86129,
-        liquidity: 1517882343751509868544,
-        currentSqrtP: 5602277097478614198912276234240,
-        shouldTransferInCallback: true,
-        mintLiqudity: true
-    });
-    }
-    
     function setupTestCase(TestCaseParams memory params)
     internal
     returns (uint256 poolBalance0, uint256 poolBalance1)
@@ -78,8 +62,18 @@ import "forge-std/Test.sol";
         token1.transfer(msg.sender, amount1);
     }
 }
-     function testAll(TestCaseParams memory params) public{
-        params=testMintSuccess();
+     function testAll() public{
+       TestCaseParams memory params = TestCaseParams({
+        wethBalance: 1 ether,
+        usdcBalance: 5000 ether,
+        currentTick: 85176,
+        lowerTick: 84222,
+        upperTick: 86129,
+        liquidity: 1517882343751509868544,
+        currentSqrtP: 5602277097478614198912276234240,
+        shouldTransferInCallback: true,
+        mintLiqudity: true
+    });
     (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
      uint256 expectedAmount0 = 0.998976618347425280 ether;
      uint256 expectedAmount1 = 5000 ether;
@@ -93,22 +87,22 @@ import "forge-std/Test.sol";
     expectedAmount1,
     "incorrect token1 deposited amount"
      );
-     assertEq(token0.balanceOf(address(pool)), expectedAmount0);
-     assertEq(token1.balanceOf(address(pool)), expectedAmount1);
+     assertEq(token0.balanceOf(address(pool)), expectedAmount0, "mistake96" );
+     assertEq(token1.balanceOf(address(pool)), expectedAmount1,"mistake97");
      bytes32 positionKey = keccak256(
     abi.encodePacked(address(this), params.lowerTick, params.upperTick)
     );
     uint128 posLiquidity = pool.positions(positionKey);
-    assertEq(posLiquidity, params.liquidity);
+    assertEq(posLiquidity, params.liquidity,"mistake102");//没有初始化 position的update
     (bool tickInitialized, uint128 tickLiquidity) = pool.ticks(
      params.lowerTick
     );
-assertTrue(tickInitialized);
-assertEq(tickLiquidity, params.liquidity);
+assertTrue(tickInitialized,"mistake106");//没有初始化 tick的update函数
+assertEq(tickLiquidity, params.liquidity,"mistake107");//与上面连带
 
 (tickInitialized, tickLiquidity) = pool.ticks(params.upperTick);
-assertTrue(tickInitialized);
-assertEq(tickLiquidity, params.liquidity);
+assertTrue(tickInitialized,"mistake110");//同上
+assertEq(tickLiquidity, params.liquidity,"mistake111");//同上
 (uint160 sqrtPriceX96, int24 tick) = pool.slot0();
 assertEq(
     sqrtPriceX96,
@@ -119,7 +113,7 @@ assertEq(tick, 85176, "invalid current tick");
 assertEq(
     pool.liquidity(),
     1517882343751509868544,
-    "invalid current liquidity"
+    "invalid current liquidity"//该流动性压根就没有初始化
 );
      }
 }
